d9911649279b387d899b26c02cf1c8c59b318cdf
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-01-04 15:20:37 -0700

    setting up google oauth

--- src/server/config/config_passport.js
+++ src/server/config/config_passport.js
@@ -1,13 +1,16 @@
 var Passport = require('passport'),
-    Google_Strategy = require('passport-google').Strategy;
+    Google_Strategy = require('passport-google-oauth').Strategy;
 
 module.exports = function(app, config) {
     Passport.use(new Google_Strategy({
-        returnURL: '',
-        realm: ''
-    }, function(identifier, profile, done){
+        clientID: 'hi',
+        clientSecret: 'cool',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    }, function(accessToken, refreshToken, profile, done){
         //The identifier is specific to the user from google
+        console.log('Google Returned: ',profile);
         //The profile will have specific profile information
+        return done(null, {message: 'HI'});
     }));
     
     
--- src/server/config/routes_auth.js
+++ src/server/config/routes_auth.js
@@ -1,3 +1,15 @@
+var Passport = require('passport');
+
 module.exports = function(app) {
-    
+    // Redirect the user to Google for authentication.  When complete, Google
+    // will redirect the user back to the application at
+    //     /auth/google/return
+    app.get('/auth/google', Passport.authenticate('google'));
+
+    // Google will redirect the user to this URL after authentication.  Finish
+    // the process by verifying the assertion.  If valid, the user will be
+    // logged in.  Otherwise, authentication has failed.
+    app.get('/auth/google/callback',
+    Passport.authenticate('google', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
 }
