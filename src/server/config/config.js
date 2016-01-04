dc408049e90a224955de3aab1f7001f076933ee1
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-01-04 15:29:29 -0700

    updated gitignore to ignore configuration file

--- src/server/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	port: 3000,
-	mongo_Uri: 'mongodb://localhost:27017',
-    session_secret: 'fasdfasdf',
-    google_Oauth: {
-        Client_Id: '',
-        Client_Secret: ''
-    }
-}
