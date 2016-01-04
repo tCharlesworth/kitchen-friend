dc0daf8b5321cf2b2841bdada71923a2742da189
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-01-04 15:21:57 -0700

    more setup for google oauth

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,5 +1,9 @@
 module.exports = {
 	port: 3000,
 	mongo_Uri: 'mongodb://localhost:27017',
-    session_secret: 'fasdfasdf'
+    session_secret: 'fasdfasdf',
+    google_Oauth: {
+        Client_Id: '',
+        Client_Secret: ''
+    }
 }
