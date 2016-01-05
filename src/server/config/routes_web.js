7f4d0899103894c13b8815fb7f52fddb10cdc2b6
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-01-05 14:37:45 -0700

    Setup new recipe endpoint and mongoose functions

--- src/server/config/routes_web.js
+++ src/server/config/routes_web.js
@@ -3,4 +3,5 @@ var recipeCtrl = require('../controllers/recipeCtrl');
 module.exports = function(app) {
     /*****Endpoints*****/
     app.get('/recipes', recipeCtrl.getRecipesByUserId);
+    app.post('/recipes', recipeCtrl.createNewRecipe);
 }
--- src/server/controllers/recipeCtrl.js
+++ src/server/controllers/recipeCtrl.js
@@ -12,5 +12,27 @@ module.exports = {
                 res.json({recipes: result.recipes})
             }
         });
+    },
+    
+    //VERIFY LOGGED IN FIRST
+    createNewRecipe: function(req, res) {
+        var recipeData = req.body;
+        //Create the new Recipe
+        Recipe.create(recipeData, function(createErr, newRecipe){
+            if(createErr) {
+                res.status(500).json(createErr);
+            } else {
+                //New recipe has been created. Lets add the id to the
+                //current user
+                User.find({_id: req.user._id}).update({$push: {recipes: newRecipe._id}}).exec(function(updateErr, updatedUser) {
+                    if(updateErr) {
+                        res.status(500).json(updateErr);
+                    } else {
+                        //Success. Return the new recipe
+                        res.json({recipe: newRecipe});
+                    }
+                })
+            }
+        });
     }
 };
