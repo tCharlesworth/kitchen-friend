2b22d01c394f911160e49ecb51765fce40839ba3
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-01-08 22:37:20 -0700

    started user and link directives for the new recipe page

---
+++ src/www/js/directives/newLinkRecipe.js
@@ -0,0 +1,5 @@
+angular.module('kitchenFriend').directive('newLinkRecipe', function() {
+    return {
+      templateUrl: 'html/templates/newLinkRecipe.html'  
+    };
+});
---
+++ src/www/js/directives/newUserRecipe.js
@@ -0,0 +1,5 @@
+angular.module('kitchenFriend').directive('newUserRecipe', function() {
+    return {
+        templateUrl: 'html/templates/newUserRecipe.html'
+    }
+})
