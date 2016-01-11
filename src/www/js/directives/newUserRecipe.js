0ff3d4474449edb86ef45fd4400fa2832ac61699
Tim Charlesworth <tim_charlesworth@outlook.com>
2016-01-10 19:31:44 -0700

    added icons and other css for the new user recipe directive

--- src/www/js/directives/newUserRecipe.js
+++ src/www/js/directives/newUserRecipe.js
@@ -1,5 +1,28 @@
 angular.module('kitchenFriend').directive('newUserRecipe', function() {
     return {
-        templateUrl: 'html/templates/newUserRecipe.html'
+        templateUrl: 'html/templates/newUserRecipe.html',
+        link: function(scope, ele, attrs) {
+            scope.newRecipe = {
+                ingredients: [],
+                directions: []
+            };
+            
+            scope.addIngredient = function(newIngredient) {
+                scope.newRecipe.ingredients.push(newIngredient);
+                scope.newIngredient = '';
+            };
+            
+            scope.addDirection = function(newDirection) {
+                scope.newRecipe.directions.push(newDirection);
+                scope.newDirection = '';
+            };
+            
+            scope.saveRecipe = function(newRecipe) {
+                //Save here
+                $('#save-new-user-recipe-button')
+                    .text('Saving...');
+                
+            };
+        }
     }
-})
+});
